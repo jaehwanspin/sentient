2aaf0c5d6896a1645d87a91be22de82ef14622f1
Jin <jaehwanspin@gmail.com>
2022-03-25 14:43:50 +0900

    first

---
+++ include/sentient/core.hpp
@@ -0,0 +1,11 @@
+#ifndef __SENTIENT_CORE_HPP__
+#define __SENTIENT_CORE_HPP__
+
+namespace sentient
+{
+
+
+
+}
+
+#endif
---
+++ include/sentient/core/types.hpp
@@ -0,0 +1,11 @@
+#ifndef __SENTIENT_CORE_TYPES_HPP__
+#define __SENTIENT_CORE_TYPES_HPP__
+
+namespace sentient
+{
+
+
+
+}
+
+#endif
